1d9f4151d8d34cec70678f8fd4e6f1a97a7d784a
tkiviharju <tomas.kiviharju@gmail.com>
2019-12-23 11:52:36 +0200

    3.5 & 3.6

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const bodyParser = require('body-parser');
 const persons = require('./persons.json');
 const fs = require('fs');
 
+
 app.use(bodyParser.json());
 
+
+const save = (newPersons) => fs.writeFileSync('./persons.json', JSON.stringify(newPersons, null, 4));
+
+
+const generateId = () => Math.floor(Math.random() * 999999999);
+
+
 app.get('/', (req, res) => res.redirect('/info'));
+
 
 app.get('/info', (req, res) => {
 	const message = `Phonebook has info for ${persons.length} people <br><br> ${new Date()}`;
 	return res.send(message);
 });
 
+
 app.get('/api/persons', (req, res) => {
 	return res.send(persons);
 });
+
 
 app.get('/api/persons/:id', (req, res) => {
 	const { id } = req.params;
@@ -24,13 +35,34 @@ app.get('/api/persons/:id', (req, res) => {
 	return person ? res.status(200).send(person) : res.sendStatus(404);
 });
 
+
+app.post('/api/persons', (req, res) => {
+	const { name, number } = req.body;
+	if (!name || !number){
+		return res.status(400).json({error: 'name or number missing'});
+	}
+	const nameAlreadyInPersons = persons.find(person => person.name.toLowerCase() === name.toLowerCase());
+	if (nameAlreadyInPersons){
+		return res.status(400).json({error: 'name must be unique'});
+	}
+	const id = generateId();
+	const person = { name, number, id };
+
+	const newPersons = persons.concat(person);
+	save(newPersons);
+	
+	return res.status(201).send(person);
+});
+
+
 app.delete('/api/persons/:id', (req, res) => {
 	const { id } = req.params;
 	const newPersons = persons.filter(person => person.id !== parseInt(id));
-	fs.writeFileSync('./persons.json', JSON.stringify(newPersons, null, 4));
-	
+	save(newPersons);
+
 	return res.sendStatus(204);
 });
+
 
 const PORT = 3001
 app.listen(PORT, () => {
