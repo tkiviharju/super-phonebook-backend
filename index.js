da0439c331b9c3da62a65af69759f7019fc0b5ac
tkiviharju <tomas.kiviharju@gmail.com>
2019-12-23 11:20:55 +0200

    3.3

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.get('/api/persons', (req, res) => {
 	return res.send(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+	const { id } = req.params;
+	
+	const person = persons.find(person => person.id === parseInt(id));
+	return person ? res.status(200).send(person) : res.sendStatus(404);
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
