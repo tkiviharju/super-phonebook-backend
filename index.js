e60a64c7ed4aa9fc221b07fd3c2abecd5531bc18
tkiviharju <tomas.kiviharju@gmail.com>
2019-12-23 11:10:58 +0200

    3.2

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const persons = require('./persons.json');
+const { persons } = require('./persons.json');
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.redirect('/api/persons'));
+app.get('/', (req, res) => res.redirect('/info'));
+
+app.get('/info', (req, res) => {
+	const message = `Phonebook has info for ${persons.length} people <br><br> ${new Date()}`;
+	return res.send(message);
+});
 
 app.get('/api/persons', (req, res) => {
-	res.send(persons);
+	return res.send(persons);
 });
 
 const PORT = 3001
