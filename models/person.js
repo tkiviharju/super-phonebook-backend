acd1ca7ed880969027fe923697e5ab511a87f506
tkiviharju <tomas.kiviharju@gmail.com>
2019-12-28 09:55:17 +0200

    3.20

--- models/person.js
+++ models/person.js
@@ -11,8 +11,8 @@ mongoose
   	.catch(error => console.log('error connecting to MongoDB:', error.message));
 
 const personSchema = new mongoose.Schema({
-	name: { type: String, required: true, unique: true },
-	number: { type: String, required: true, unique: true },
+	name: { type: String, required: true, unique: true, minlength: 3 },
+	number: { type: String, required: true, unique: true, minlength: 8 }
 });
 
 personSchema.plugin(validator);
